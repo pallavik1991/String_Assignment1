b124ce6b4dd50bbf5053f1c0c883e157a41e0b70
pallavik1991 <pallu.katti@gmail.com>
2017-03-05 03:31:21 -0800

    Add files via upload

---
+++ Assignment7_1.java
@@ -0,0 +1,9 @@
+class Assignment7_1
+{
+	public static void main(String[] args)
+	{
+		int a=10;
+                String b=String.valueOf(a);
+                System.out.println("Converted value from int to String="+b);
+	}
+}
